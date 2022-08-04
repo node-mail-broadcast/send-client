f114b6860cabc79d38522252bae6834640fdb861
Nico Wagner <info@ni-wa.de>
2022-08-04 17:39:45 +0200

    updated sendEmail function

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ export class EmailSenderClient {
    * @author Nico W.
    * @async
    * @version 1.0.0
-   * @since 0.0.2 04.07.2021
+   * @since 0.0.2 04.07.2022
    */
   private createChannelAndAssertQueue(): Promise<ConfirmChannel> {
     return new Promise((resolve) => {
@@ -46,13 +46,24 @@ export class EmailSenderClient {
     });
   }
 
-  public sendEmail(content: any) {
+  /**
+   * This function will send emails-infos into a queue which will be sent from another client
+   * @param data - Data with email info to send
+   * @author Nico W.
+   * @async
+   * @version 1.0.0
+   * @since 04.08.2022
+   */
+  public sendEmail(data: {
+    content: Record<string, string>;
+    templateName: string;
+  }) {
     return new Promise((resolve, reject) => {
       this.channel.sendToQueue(
         this.queue,
-        Buffer.from(content),
+        Buffer.from(JSON.stringify(data)),
         {},
-        (err: any, ok) => {
+        (err, ok) => {
           if (err) return reject(err);
           resolve(ok);
         }
